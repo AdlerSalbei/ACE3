dde90e2fe6337d50bd41ec2b95f4a5410dba20d3
Salbei <salbei@gruppe-adler.de>
2018-12-02 20:06:10 +0100

    Added anims

--- addons/captives/CfgMoves.hpp
+++ addons/captives/CfgMoves.hpp
@@ -1,4 +1,12 @@
 class CfgMovesBasic {
+  	class default;
+   	class DefaultDie;
+   	class ManActions
+   	{
+  		AnimCableStandStart[] = {"AnimCableStandStart", "gesture"};
+  		AnimCableStandLoop[] = {"AnimCableStandLoop", "gesture"};
+  		AnimCableStandEnd[] = {"AnimCableStandEnd", "gesture"};
+   	};
     class Actions {
         class CivilStandActions;
         class ACE_CivilStandHandcuffedActions: CivilStandActions {
@@ -26,9 +34,107 @@ class CfgMovesBasic {
     };
 };
 
-class CfgMovesMaleSdr: CfgMovesBasic {
-    class StandBase;
-    class States {
+class CfgGesturesMale
+{
+  class default;
+ 	class States
+ 	{
+		class AnimCableStandStart : default
+ 		{
+ 			file = "bnae_animations\anim_cable_stand_start.rtm";
+			speed=1;
+			looped=0;
+			mask = "handsWeapon_pst";
+			enableDirectControl = 0;
+			leftHandIKCurve[] = {0};
+			RightHandIKCurve[] = {0};
+			weaponIK = 0;
+			forceAim = 1;
+			leaning = "empty";
+			aimingBody = "empty";
+			aiming = "empty";
+			head = "empty";
+			disableWeapons = 1;
+			disableWeaponsLong = 1;
+			headBobMode = 1;
+			headBobStrength = 0.2;
+			canPullTrigger = 0;
+			showHandGun = 0;
+			showItemInRightHand = 0;
+			variantsPlayer[] = {};
+			variantsAI[] = {};
+			weaponLowered=0;
+ 		};
+		class AnimCableStandLoop : AnimCableStandStart
+ 		{
+ 			file = "bnae_animations\anim_cable_stand_loop.rtm";
+			speed=0.06;
+			looped=1;
+ 		};
+		class AnimCableStandEnd : AnimCableStandStart
+ 		{
+ 			file = "bnae_animations\anim_cable_stand_end.rtm";
+ 		};
+	};
+};
+
+class CfgMovesMaleSdr: CfgMovesBasic
+{
+  skeletonName = "OFP2_ManSkeleton";
+	gestures = "CfgGesturesMale";
+	access = 1;
+  class StandBase;
+	class States
+  {
+  		class default;
+  		class AnimCableCrouchStart : default
+   		{
+   			file = "bnae_animations\anim_cable_crouch_start.rtm";
+  			looped = 0;
+  			speed = 1;
+  			duty = 2;
+  			mask = "BodyFullReal";
+  			enableDirectControl = 0;
+  			leftHandIKCurve[] = {0};
+  			RightHandIKCurve[] = {0};
+  			weaponIK = 0;
+  			forceAim = 1;
+  			leaning = "empty";
+  			aimingBody = "empty";
+  			aiming = "empty";
+  			head = "empty";
+  			disableWeapons = 1;
+  			disableWeaponsLong = 1;
+  			headBobMode = 1;
+  			headBobStrength = 0.2;
+  			canPullTrigger = 0;
+  			showHandGun = 0;
+  			showItemInRightHand = 0;
+  			variantsPlayer[] = {};
+  			variantsAI[] = {};
+  			weaponLowered=0;
+   		};
+  		class AnimCableCrouchLoop : AnimCableCrouchStart
+   		{
+   			file = "bnae_animations\anim_cable_crouch_loop.rtm";
+  			looped=1;
+  			speed=0.1;
+  			ConnectTo[]=
+  			{
+  				"BasicDriverDead",
+  				1
+  			};
+  			InterpolateTo[]=
+  			{
+  				"BasicDriverDead",
+  				0.1
+  			};
+   		};
+  		class AnimCableCrouchToStand : AnimCableCrouchStart
+   		{
+   			file = "bnae_animations\anim_cable_crouch_to_stand.rtm";
+   		};
+
         class AmovPercMstpSnonWnonDnon: StandBase {
             ConnectTo[] += {"ACE_AmovPercMstpSnonWnonDnon_AmovPercMstpScapWnonDnon",0.1,"ACE_AmovPercMstpSnonWnonDnon_AmovPercMstpSsurWnonDnon",0.1};
         };
